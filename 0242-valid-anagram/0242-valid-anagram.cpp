1e04b15127011c554d09fdec91d67b1490758325
ajiteshsharma-hub <ajitesh2405sharma@gmail.com>
2026-04-23 00:35:49 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,12 +4,15 @@ public:
         if(s.length() != t.length()) return false;
         int freq[26] = {0};
         for(int i = 0; i < s.length(); i++){
+            //Increament frequency for each character in s
             freq[s[i] - 'a']++;
         }
         for(int i = 0; i< t.length(); i++){
+            //Decreament frequency for each character in t
             freq[t[i] - 'a']--;
         }
         for(int i = 0; i < 26; i++){
+            //Check if frequency is zero or not
             if(freq[i] != 0) return false;
         }
         return true;
