36210b2f07858119dd6d142ba542cd93db88e68b
ajiteshsharma-hub <ajitesh2405sharma@gmail.com>
2026-06-23 00:34:45 +0530

    Time: 0 ms (100%), Space: 11.4 MB (88.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,7 +8,9 @@ public:
         }
         int l = 0, r = 0, len = 0, maxLen = 0;
         while(r < n){
+            //If the current character is already in the hash table
             if(hash[s[r]] != -1){
+                //Move the left pointer to the right of the last occurence of s[r]
                 l = max(hash[s[r]] + 1, l);
             }
             len = r - l + 1;
